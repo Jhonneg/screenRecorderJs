404d27862f17b472b7aceb5fca9c9560ced6b7c8
Jhonne <jonee4339@hotmail.com>
2025-11-09 23:14:50 -0300

    added video card code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
 
-function Page() {
+export default function Page() {
   return (
     <main className="wrapper page">
-      <Header />
-      <h1 className="text-2xl font-carla">Welcome to Loom Clone</h1>
+      <Header title="All Videos" subHeader="Public Library" />
+      <VideoCard
+        id="1"
+        title="Snapchat"
+        thumbnail="/assets/samples/thumbnail (1).png"
+        createdAt={new Date("2025-05-01")}
+        userImg="/assets/images/jason.png"
+        username="Jason"
+        views={10}
+        visibility="public"
+        duration={156}
+      />
     </main>
   );
 }
-
-export default Page;
